9cedaa385bbf9ce3e7725a6a4c46ac105021f28f
Andy Wong <awong.cm@gmail.com>
2014-07-14 23:13:35 +1000

    New extra checkout functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,6 +61,34 @@ class ProductsController < ApplicationController
     end
   end
 
+  def who_bought
+      @product = Product.find(params[:id])
+      @latest_order = @product.orders.order(:updated_at).last
+      if stale?(@latest_order)
+        respond_to do |format|
+          format.atom
+          format.xml { render( :xml => @product.to_xml(
+           :only => [ :title, :updated_at ],
+           :skip_types => true,
+           :include => {
+              :orders => {
+                 :except => [ :created_at, :updated_at ],
+                 :skip_types => true,
+                 :include => {
+                    :line_items => {
+                       :skip_types => true,
+                       :except => [ :created_at, :updated_at, :cart_id, :order_id ]
+                    }
+                 }
+              }
+           }
+        )) }
+          format.json { render json: @product.to_json(:include => :orders) }
+          format.html #who_bought.html.erb
+        end
+      end
+    end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
@@ -72,13 +100,5 @@ class ProductsController < ApplicationController
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
 
-    def who_bought
-      @product = Product.find(params[:id])
-      @latest_order = @product.orders.order(:updated_at).last
-      if stale?(@latest_order)
-        respond_to do |format|
-          format.atom
-        end
-      end
-    end
+
 end
